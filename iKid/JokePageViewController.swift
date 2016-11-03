9ad77e3b316755bd2d464996ebd53f1d50b41261
Alexis Anand <alexis@alexisanand.com>
2016-11-03 14:37:42 -0700

    Initial commit

---
+++ iKid/JokePageViewController.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  iKid
+//
+//  Created by iGuest on 11/3/16.
+//  Copyright © 2016 Alexis Anand. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class JokePageViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        modalTransitionStyle = .flipHorizontal
+    }
+}
